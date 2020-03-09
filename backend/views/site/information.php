78e98a1173fc25df5923746dc5fe867428dba0f9
AminDiyass2 <AminDiyasSerikuly>
2020-03-09 23:39:40 +0600

    fix bug from last testing results

--- backend/views/site/information.php
+++ backend/views/site/information.php
@@ -45,7 +45,7 @@ use common\models\BookSentences;
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Количество прочтанных
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Количество прочитанных
                             книг
                         </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?=
@@ -128,8 +128,7 @@ use common\models\BookSentences;
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Количество памяти аудио и
-                            текстов
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Количество памяти книг
                         </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $sizeBook ?></div>
                     </div>
